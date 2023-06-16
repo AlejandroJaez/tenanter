b9f9e56f3d2ebbb7a10c76ed13571eba444a2dbc
Alejandro Hernandez <alejandrojaez@outlook.com>
2023-06-15 19:44:07 -0600

    upd

---
+++ repairs/utils.py
@@ -0,0 +1,19 @@
+
+
+REGISTRADO = "FR"
+RECIBIDO = "SO"
+REVISADO = "JR"
+ESPERA = "SR"
+REPARADO = "GR"
+IRREPARABLE = ""
+ENTREGADO = ""
+
+YEAR_IN_SCHOOL_CHOICES = [
+    (REGISTRADO, "Registrado"),
+    (RECIBIDO, "Recibido"),
+    (REVISADO, "Revisado"),
+    (ESPERA, "Espera"),
+    (REPARADO, "Reparado"),
+    (IRREPARABLE, "Irreparable"),
+    (ENTREGADO, "Entregado"),
+]
